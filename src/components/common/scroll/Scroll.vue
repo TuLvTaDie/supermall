278cbe6197cdbcc2e648cdb2a7dfa375d413ee17
TuLvTaDie <65690679+TuLvTaDie@users.noreply.github.com>
2022-03-23 17:06:47 +0800

    '首页'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Scroll",
+    props: {
+      probeType: {
+        type: Number,
+        default: 0
+      },
+      pullUpLoad: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data() {
+      return {
+        scroll: null,
+      }
+    },
+    mounted() {
+      // 1.创建BScroll对象
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        click: true,//必须设置click:true，那么div才能监听点击
+        probeType: this.probeType,
+        pullUpLoad: this.pullUpLoad
+      })
+      // 2.监听滚动的位置
+      if (this.probeType === 2 || this.probeType ===3) {
+        this.scroll.on('scroll', (position) => {
+          this.$emit('scroll', position)
+        })
+      }
+      // 3.监听上拉事件
+      if (this.pullUpLoad) {
+        this.scroll.on('pullingUp', () => {
+          this.$emit('pullingUp')
+        })
+      }
+    },
+    methods: {
+      scrollTo(x, y, time=200) {
+        this.scroll && this.scroll.scrollTo(x, y, time)
+      },
+      finishPullUp() {
+        this.scroll.finishPullUp()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
